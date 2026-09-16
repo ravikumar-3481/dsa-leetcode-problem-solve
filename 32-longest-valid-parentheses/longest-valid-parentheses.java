66c0770bf9af00ac990324272bca2be0e92aadbb
Ravi Kumar Vishwakarma <ravivish968@gmail.com>
2026-09-16 14:41:42 +0530

    Time: 5 ms (77.77%) | Memory: 46.3 MB (71.17%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -4,12 +4,16 @@ class Solution {
         stack.push(-1);
         int max_len = 0;
 
+
+
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
+
                 stack.push(i);
             } else {
                 stack.pop();
                 if (stack.isEmpty()) {
+                    
                     stack.push(i);
                 } else {
                     max_len = Math.max(max_len, i - stack.peek());
@@ -18,6 +22,6 @@ class Solution {
         }
 
         return max_len;       
-         
+
     }
 }
