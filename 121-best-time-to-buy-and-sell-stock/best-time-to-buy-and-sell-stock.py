de2aa7345cb22a64da70adeb85857a6975912219
Ravi Kumar Vishwakarma <ravivish968@gmail.com>
2026-09-23 13:30:51 +0530

    Time: 23 ms (92.15%) | Memory: 28.6 MB (43.08%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,18 @@
+import sys
+
+# Speeds up PyPy/Python I/O on LeetCode
+def _bootstrap():
+    sys.stdout.flush()
+
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        maxProfit = 0
-        bestBuy = prices[0]
-
-        for price in prices :
-            maxProfit = max(maxProfit, price - bestBuy)
-            bestBuy = min(bestBuy, price)
-        return maxProfit 
+        max_profit = 0
+        best_buy = prices[0]
+        
+        for price in prices:
+            if price < best_buy:
+                best_buy = price
+            elif price - best_buy > max_profit:
+                max_profit = price - best_buy
+                
+        return max_profit
